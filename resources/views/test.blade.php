7f86d96e8b8eb332a99f0418c258a6672dc1f39d
Alejandro Estévez <programacion@compusof.com>
2023-10-13 15:44:23 +0200

    First Commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>DEVSTRAGRAM - @yield('name')</title>
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<body class="bg-gray-100">
+    <header class="p-5 border-b bg-white shadow">
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="font-black text-3xl">HOLA2</h1>
+        </div>
+        {{ now()->year }}
+    </header>
+    <nav>Hola</nav>
+</body>r
+
+</html>
